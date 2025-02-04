74af077887d5fbb193493e1bbe3a7b071d8b348b
Reiya-Sakuya <xjt13543870361@163.com>
2025-02-04 18:16:59 +0800

    代码实现pid控制器

--- source/motor_control/Core/Inc/pid.h
+++ source/motor_control/Core/Inc/pid.h
@@ -1,10 +1,16 @@
-    typedef struct pid
-    {
-        float kp;
-        float ki;
-        float dk;
-    }pid_t;
+#ifndef PID_H
+#define PID_H
+typedef struct pid
+{
+    float kp;
+    float ki;
+    float kd;
+    float integral_top;
+    float integral_bottom;
+}pid_t;
 
-    float pid_init(float kp,float ki,float kd);
+float pid_init(pid_t *args,float arg_kp,float arg_ki,float arg_kd,float top,float bottom);
 
-    float calc_pid(float err);
+float calc_pid(pid_t *args,float err);
+
+#endif
--- source/motor_control/Core/Src/pid.c
+++ source/motor_control/Core/Src/pid.c
@@ -1,4 +1,28 @@
-float pid_init(float kp,float ki,float kd)
+#include "math.h"
+#include "pid.h"
+#include "main.h"
+
+float prev_err, intergral_err=0;
+extern float dt;
+
+float pid_init(pid_t *args,float arg_kp,float arg_ki,float arg_kd,float top,float bottom)
 {
+    args->kp = arg_kp;
+    args->ki = arg_ki;
+    args->kd = arg_kd;
+    args->integral_top = top;
+    args->integral_bottom = bottom;
     return 0;
+}
+
+float calc_pid(pid_t *args,float err)
+{
+    float sum,d_err;
+    d_err = err-prev_err;
+    intergral_err += err*dt;
+    if(intergral_err>(args->integral_top)) intergral_err = args->integral_top;
+    else if (intergral_err<(args->integral_bottom)) intergral_err = args->integral_bottom;
+    sum = ((args->kp)*err) + ((args->ki)*intergral_err) + ((args->kd)*d_err/dt);
+    prev_err = err;
+    return sum;
 }
